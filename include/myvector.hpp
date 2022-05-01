73050c7cee4a9f8efb46dd5f92a38c87343b5376
renhaofan <yqykrhf@163.com>
2022-05-01 17:10:41 +0800

    add > operator and 3D vector

--- include/myvector.hpp
+++ include/myvector.hpp
@@ -2,9 +2,9 @@
  *  Copyright (C) All rights reserved.
  *  @file myvector.hpp
  *  @brief 2D,3D,4D vector simple implementation
- *  @author haofan ren
- *  @version 0.0
- *  @date 2022-4-26
+ *  @author haofan ren, yqykrhf@163.com
+ *  @version beta 0.0
+ *  @date 2222-2-26
  *  @note comment ref:https://www.cnblogs.com/huipengly/p/9257012.html
  *  @note comment ref:https://www.doxygen.nl/manual/docblocks.html
  */
@@ -23,22 +23,34 @@ typedef double scalar;
 typedef float scalar;
 #endif
 
+
+
+
 /**
  *  @brief Macro about float number equal operation.
  */
 #define MYEPSILON 1e-6
 
+/**
+ *  @brief Macro convert radians to degree
+ */
+#define RAD2DEG 180.0 / 3.141592653579
+
+/**
+ *  @brief Macro convert degree to radians
+ */
+#define DEG2RAD 3.141592653579 / 180.0
 
 /**
  *  @brief Vector2 class used for 2D vector.
  */
 struct Vector2 {
-   /**
+    /**
     * data in 2D vector.
     */
     scalar x, y;
 
-   /**
+    /**
      *  Create a new 2D vector object (0, 0).
      *  @brief Default constructor.
      *  @see Vector2(scalar _x, scalar _y)
@@ -46,7 +58,7 @@ struct Vector2 {
     Vector2() :
         x(0), y(0){};
     /**
-     *  Create a new 2D vector object (0, 0).
+     *  Create a new 2D vector object (_x, _y).
      *  @brief Default constructor.
      *  @see Vector2()
      */
@@ -58,7 +70,7 @@ struct Vector2 {
     /** @brief Return length of the vector. */
     scalar length() const;
     /** @brief Alias of length(). */
-    scalar norm() const;
+    scalar l2norm() const;
     /** @brief Euclidean distance bewteen two vectros. */
     scalar distance(const Vector2& vec) const;
     /** @brief Return the normalized vector. */
@@ -107,7 +119,6 @@ struct Vector2 {
      */
     Vector2& operator*=(const Vector2& rhs);
 
-
     /**
      *  @brief Binary operator, 2D vector times scalar.
      *  @see operator*=(const scalar scale)
@@ -118,7 +129,6 @@ struct Vector2 {
      *  @see operator*(const scalar scale) const
      */
     Vector2& operator*=(const scalar scale);
-
 
     /**
      *  @brief Binary operator, 2D vector divided by scalar.
@@ -149,8 +159,16 @@ struct Vector2 {
      *  @brief comparsion overloading, for the purpose of sort.
      *  @param rhs another 2D vector object.
      *  @return Whether 2D vector object is less than another.
+     *  @see operator>(const Vector2& rhs) const;
      */
     bool operator<(const Vector2& rhs) const;
+    /**
+     *  @brief comparsion overloading, for the purpose of sort.
+     *  @param rhs another 2D vector object.
+     *  @return Whether 2D vector object is more than another.
+     *  @see operator<(const Vector2& rhs) const;
+     */
+    bool operator>(const Vector2& rhs) const;
 
     /**
      *  @brief index[] overloading.
@@ -175,4 +193,164 @@ struct Vector2 {
      *  @return std::ostream object
      */
     friend std::ostream& operator<<(std::ostream& os, const Vector2& vec);
+};
+
+/**
+ *  @brief Vector3 class used for 3D vector.
+ */
+struct Vector3 {
+    /**
+    * data in 3D vector.
+    */
+    scalar x, y, z;
+
+    /**
+     *  Create a new 3D vector object (0, 0, 0).
+     *  @brief Default constructor.
+     *  @see Vector3(scalar _x, scalar _y)
+     */
+    Vector3() :
+        x(0), y(0), z(0){};
+    /**
+     *  Create a new 3D vector object (_x, _y, _z).
+     *  @brief Default constructor.
+     *  @see Vector3()
+     */
+    Vector3(scalar _x, scalar _y, scalar _z) :
+        x(_x), y(_y), z(_z){};
+
+    /** @brief Set member variables x,y,z. */
+    void set(scalar _x, scalar _y, scalar _z);
+    /** @brief Return length of the vector. */
+    scalar length() const;
+    /** @brief Alias of length(). */
+    scalar l2norm() const;
+    /** @brief Euclidean distance bewteen two vectros. */
+    scalar distance(const Vector3& vec) const;
+    /** @brief angle bewteen two vectros.
+     *  @return angle in degree [0, 180].
+     */
+    scalar angle(const Vector3& vec) const;
+    /** @brief Return the normalized vector. */
+    Vector3 normalized() const;
+    /** @brief In-place operation, normalized vector. */
+    void normalize();
+    /** @brief Dot product between two vectors. */
+    scalar dot(const Vector3& vec) const;
+    /** @brief cross product between two vectors. */
+    Vector3 cross(const Vector3& rhs) const;
+    /** @brief Determine if the 3D vector object is similar to the other. */
+    bool equal(const Vector3& vec, scalar e = MYEPSILON) const;
+
+    /** @brief Unary operator, negate a 3D vector. */
+    Vector3 operator-() const;
+
+    /**
+     *  @brief Binary operator, addition of two 3D vector.
+     *  @see operator+=(const Vector3& rhs)
+     */
+    Vector3 operator+(const Vector3& rhs) const;
+    /**
+     *  @brief Abbr binary operator, addition of two 3D vector.
+     *  @see operator+(const Vector3& rhs)
+     */
+    Vector3& operator+=(const Vector3& rhs);
+
+    /**
+     *  @brief Binary operator, subtraction of two 3D vectors.
+     *  @see operator-=(const Vector3& rhs)
+     */
+    Vector3 operator-(const Vector3& rhs) const;
+
+    /**
+     *  @brief Abbr binary operator, subtraction of two 3D vectors.
+     *  @see operator-(const Vector3& rhs) const
+     */
+    Vector3& operator-=(const Vector3& rhs);
+
+    /**
+     *  @brief Binary operator, multiplication between two 3D vectors.
+     *  @see operator*=(const Vector3& rhs)
+     */
+    Vector3 operator*(const Vector3& rhs) const;
+    /**
+     *  @brief Abbr binary operator, multiplication between two 3D vectors.
+     *  @see operator*(const Vector3& rhs) const
+     */
+    Vector3& operator*=(const Vector3& rhs);
+
+    /**
+     *  @brief Binary operator, 3D vector times scalar.
+     *  @see operator*=(const scalar scale)
+     */
+    Vector3 operator*(const scalar scale) const;
+    /**
+     *  @brief Abbr binary operator, 3D vector times scalar.
+     *  @see operator*(const scalar scale) const
+     */
+    Vector3& operator*=(const scalar scale);
+
+    /**
+     *  @brief Binary operator, 3D vector divided by scalar.
+     *  @see operator/=(const scalar scale)
+     */
+    Vector3 operator/(const scalar scale) const;
+    /**
+     *  @brief Abbr binary operator, 3D vector divided by scalar.
+     *  @see operator/(const scalar scale) const
+     */
+    Vector3& operator/=(const scalar scale);
+
+    /**
+     *  @brief Equivalence overloading, exact comparsion, no epsilon.
+     *  @param rhs another 3D vector object.
+     *  @return Whether the two 3D vector objects are the same.
+     *  @see operator!=(const Vector3& rhs) const;
+     */
+    bool operator==(const Vector3& rhs) const;
+    /**
+     *  @brief Inequivalence overloading, exact comparsion, no epsilon.
+     *  @param rhs another 3D vector object.
+     *  @return Whether the two 3D vector objects are not the same.
+     *  @see operator==(const Vector3& rhs) const;
+     */
+    bool operator!=(const Vector3& rhs) const;
+    /**
+     *  @brief comparsion overloading, for the purpose of sort.
+     *  @param rhs another 3D vector object.
+     *  @return Whether 3D vector object is less than another.
+     *  @see operator>(const Vector3& rhs) const
+     */
+    bool operator<(const Vector3& rhs) const;
+    /**
+     *  @brief comparsion overloading, for the purpose of sort.
+     *  @param rhs another 3D vector object.
+     *  @return Whether 3D vector object is more than another.
+     *  @see operator>(const Vector3& rhs) const
+     */
+    bool operator>(const Vector3& rhs) const;
+
+    /**
+     *  @brief index[] overloading.
+     *  @param index 3D vector w, w[index].
+     *  @return member variable, w[index], w[0], w[1]
+     */
+    scalar operator[](int index) const;
+    /**
+     *  @brief index[] overloading.
+     *  @param index 3D vector w, w[index].
+     *  @return member variable, w[index]
+     */
+    scalar& operator[](int index);
+
+    /**
+     *  @brief scalar times 3D vector.
+     *  @return 3D vector.
+     */
+    friend Vector3 operator*(const scalar a, const Vector3 vec);
+    /**
+     *  @brief << overloading.
+     *  @return std::ostream object
+     */
+    friend std::ostream& operator<<(std::ostream& os, const Vector3& vec);
 };
--- myvector.cpp
+++ myvector.cpp
@@ -13,13 +13,13 @@ scalar Vector2::length() const {
 #endif
 }
 
-scalar Vector2::norm() const {
+scalar Vector2::l2norm() const {
     return length();
 }
 
 scalar Vector2::distance(const Vector2& vec) const {
 #ifdef USING_FLOAT64
-    return sqrt(x * x + y * y);
+    return sqrt((vec.x - x) * (vec.x - x) + (vec.y - y) * (vec.y - y));
 #else
     return sqrtf((vec.x - x) * (vec.x - x) + (vec.y - y) * (vec.y - y));
 #endif
@@ -37,8 +37,13 @@ Vector2 Vector2::normalized() const {
 
 void Vector2::normalize() {
     scalar len = length();
-    this->x = x / len;
-    this->y = y / len;
+    if (len < MYEPSILON) {
+        std::cerr << "Vector2d length is too small" << std::endl;
+        throw "Division by zero condition! norm less than EPSILON";
+    } else {
+        this->x = x / len;
+        this->y = y / len;
+    }
 }
 
 scalar Vector2::dot(const Vector2& vec) const {
@@ -52,10 +57,6 @@ bool Vector2::equal(const Vector2& vec, scalar e) const {
 Vector2 Vector2::operator-() const {
     return Vector2(-x, -y);
 }
-
-//Vector2& Vector2::operator=(Vector2& rhs) const {
-//    return rhs;
-//}
 
 Vector2 Vector2::operator+(const Vector2& rhs) const {
     return Vector2(x + rhs.x, y + rhs.y);
@@ -116,7 +117,7 @@ Vector2& Vector2::operator/=(const scalar scale) {
 }
 
 bool Vector2::operator==(const Vector2& rhs) const {
-    return !((x != rhs.x) || (y != rhs.y));
+    return ((x == rhs.x) && (y == rhs.y));
 }
 
 bool Vector2::operator!=(const Vector2& rhs) const {
@@ -132,9 +133,13 @@ bool Vector2::operator<(const Vector2& rhs) const {
     return false;
 }
 
+bool Vector2::operator>(const Vector2& rhs) const {
+    return !(operator<(rhs));
+}
+
 scalar Vector2::operator[](int index) const {
-//  not very clear which case used
-//    std::cout << "not reference" << std::endl;
+    //  not very clear which case used
+    //    std::cout << "not reference" << std::endl;
     if (index > 1) {
         std::cerr << "Index out of bounds" << std::endl;
         throw "Index out of bounds, index should less than 2";
@@ -147,8 +152,8 @@ scalar Vector2::operator[](int index) const {
 }
 
 scalar& Vector2::operator[](int index) {
-//  w[0]
-//    std::cout << "reference" << std::endl;
+    //  w[0]
+    //    std::cout << "reference" << std::endl;
     if (index > 1) {
         std::cerr << "Index out of bounds" << std::endl;
         throw "Index out of bounds, index should less than 2";
@@ -161,10 +166,212 @@ scalar& Vector2::operator[](int index) {
 }
 
 Vector2 operator*(const scalar a, const Vector2 vec) {
-    return Vector2(a*vec.x, a*vec.y);
+    return Vector2(a * vec.x, a * vec.y);
 }
 
 std::ostream& operator<<(std::ostream& os, const Vector2& vec) {
     os << "(" << vec.x << ", " << vec.y << ")";
+    return os;
+}
+
+void Vector3::set(scalar _x, scalar _y, scalar _z) {
+    this->x = _x;
+    this->y = _y;
+    this->z = _z;
+}
+
+scalar Vector3::length() const {
+#ifdef USING_FLOAT64
+    return sqrt(x * x + y * y + z * z);
+#else
+    return sqrtf(x * x + y * y + z * z);
+#endif
+}
+
+scalar Vector3::l2norm() const {
+    return length();
+}
+
+scalar Vector3::distance(const Vector3& vec) const {
+#ifdef USING_FLOAT64
+    return sqrt((vec.x - x) * (vec.x - x) + (vec.y - y) * (vec.y - y) + (vec.z - z) * (vec.z - z));
+#else
+    return sqrtf((vec.x - x) * (vec.x - x) + (vec.y - y) * (vec.y - y) + (vec.z - z) * (vec.z - z));
+#endif
+}
+
+scalar Vector3::angle(const Vector3& vec) const {
+    scalar len = this->length() * vec.length();
+    if (len < MYEPSILON) {
+        std::cerr << "length is too small" << std::endl;
+        throw "Division by zero condition! norm less than EPSILON";
+    } else {
+#ifdef USING_FLOAT64
+        return acos(this->dot(vec) / len) * (RAD2DEG);
+#else
+        // acosf return  [0 ; π],
+        return acosf(this->dot(vec) / len) * (RAD2DEG);
+#endif
+    }
+}
+
+Vector3 Vector3::normalized() const {
+    scalar len = length();
+    if (len < MYEPSILON) {
+        std::cerr << "Vector2d length is too small" << std::endl;
+        throw "Division by zero condition! norm less than EPSILON";
+    } else {
+        return Vector3(x / len, y / len, z / len);
+    }
+}
+
+void Vector3::normalize() {
+    scalar len = length();
+    if (len < MYEPSILON) {
+        std::cerr << "Vector2d length is too small" << std::endl;
+        throw "Division by zero condition! norm less than EPSILON";
+    } else {
+        this->x = x / len;
+        this->y = y / len;
+        this->z = z / len;
+    }
+}
+
+scalar Vector3::dot(const Vector3& vec) const {
+    return (x * vec.x + y * vec.y + z * vec.z);
+}
+
+Vector3 Vector3::cross(const Vector3& rhs) const {
+    return Vector3(y * rhs.z - z * rhs.y, z * rhs.x - x * rhs.z, x * rhs.y - y * rhs.x);
+}
+
+bool Vector3::equal(const Vector3& vec, scalar e) const {
+    return !((std::abs(vec.x - x) > e) || (std::abs(vec.y - y) > e) || (std::abs(vec.z - z) > e));
+}
+
+Vector3 Vector3::operator-() const {
+    return Vector3(-x, -y, -z);
+}
+
+Vector3 Vector3::operator+(const Vector3& rhs) const {
+    return Vector3(x + rhs.x, y + rhs.y, z + rhs.z);
+}
+
+Vector3& Vector3::operator+=(const Vector3& rhs) {
+    x += rhs.x;
+    y += rhs.y;
+    z += rhs.z;
+    return *this;
+}
+
+Vector3 Vector3::operator-(const Vector3& rhs) const {
+    return Vector3(x - rhs.x, y - rhs.y, z - rhs.z);
+}
+
+Vector3& Vector3::operator-=(const Vector3& rhs) {
+    x -= rhs.x;
+    y -= rhs.y;
+    z -= rhs.z;
+    return *this;
+}
+
+Vector3 Vector3::operator*(const Vector3& rhs) const {
+    return Vector3(x * rhs.x, y * rhs.y, z * rhs.z);
+}
+
+Vector3& Vector3::operator*=(const Vector3& rhs) {
+    x *= rhs.x;
+    y *= rhs.y;
+    z *= rhs.z;
+    return *this;
+}
+
+Vector3 Vector3::operator*(const scalar scale) const {
+    return Vector3(x * scale, y * scale, z * scale);
+}
+
+Vector3& Vector3::operator*=(const scalar scale) {
+    x *= scale;
+    y *= scale;
+    z *= scale;
+    return *this;
+}
+
+Vector3 Vector3::operator/(const scalar scale) const {
+    if (std::abs(scale) < MYEPSILON) {
+        std::cerr << "scale too smaple" << std::endl;
+        throw "Division by zero condition! norm less than EPSILON";
+    }
+    return Vector3(x / scale, y / scale, z / scale);
+}
+
+Vector3& Vector3::operator/=(const scalar scale) {
+    if (std::abs(scale) < MYEPSILON) {
+        std::cerr << "scale too smaple" << std::endl;
+        throw "Division by zero condition! norm less than EPSILON";
+    }
+    x /= scale;
+    y /= scale;
+    z /= scale;
+    return *this;
+}
+
+bool Vector3::operator==(const Vector3& rhs) const {
+    return ((x == rhs.x) && (y == rhs.y) && (z == rhs.z));
+}
+
+bool Vector3::operator!=(const Vector3& rhs) const {
+    return ((x != rhs.x) || (y != rhs.y) || (z != rhs.z));
+}
+
+bool Vector3::operator<(const Vector3& rhs) const {
+    //! possible issue: < > comparison rather than EPISILON
+    if (x < rhs.x) return true;
+    if (x > rhs.x) return false;
+    if (y < rhs.y) return true;
+    if (y > rhs.y) return false;
+    if (z < rhs.z) return true;
+    if (z > rhs.z) return false;
+    return false;
+}
+
+bool Vector3::operator>(const Vector3& rhs) const {
+    return !(operator<(rhs));
+}
+
+scalar Vector3::operator[](int index) const {
+    //  not very clear which case used
+    //    std::cout << "not reference" << std::endl;
+    if (index > 2) {
+        std::cerr << "Index out of bounds" << std::endl;
+        throw "Index out of bounds, index should less than 2";
+    }
+    if (index < 0) {
+        std::cerr << "Index out of bounds" << std::endl;
+        throw "Index out of bounds, index should more than 0";
+    }
+    return (&x)[index];
+}
+
+scalar& Vector3::operator[](int index) {
+    //  w[0]
+    //    std::cout << "reference" << std::endl;
+    if (index > 2) {
+        std::cerr << "Index out of bounds" << std::endl;
+        throw "Index out of bounds, index should less than 3";
+    }
+    if (index < 0) {
+        std::cerr << "Index out of bounds" << std::endl;
+        throw "Index out of bounds, index should more than 0";
+    }
+    return (&x)[index];
+}
+
+Vector3 operator*(const scalar a, const Vector3 vec) {
+    return Vector3(a * vec.x, a * vec.y, a * vec.z);
+}
+
+std::ostream& operator<<(std::ostream& os, const Vector3& vec) {
+    os << "(" << vec.x << ", " << vec.y << ", " << vec.z << ")";
     return os;
 }
