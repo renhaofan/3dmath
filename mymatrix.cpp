51a67ea7c159e2e691eed720f42506da2a91510e
renhaofan <yqykrhf@163.com>
2022-05-14 23:00:58 +0800

    fix determinant matrix3

--- mymatrix.cpp
+++ mymatrix.cpp
@@ -517,7 +517,7 @@ Vector3 Matrix3::getColumn(int index) const {
 scalar Matrix3::getDeterminant() const {
     return (m[0] * (m[4] * m[8] - m[5] * m[7])
             - m[1] * (m[3] * m[8] - m[5] * m[6])
-            + m[2] * (m[3] * m[7] + m[4] * m[6]));
+            + m[2] * (m[3] * m[7] - m[4] * m[6]));
 }
 
 bool Matrix3::isRotationMatrix() const {
